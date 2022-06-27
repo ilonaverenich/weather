71658d6c0135864421619b67de113048572e3e2a
Ilona <Ilonaverenich@tut.by>
2022-06-27 17:32:53 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import {useDispatch, useSelector} from 'react-redux';
 import { axiosData } from './redux/actions/getDataAction';
 import { useEffect,useState } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Input, message } from 'antd';
+
+
 
 
 function App() {
     const [value,setValue] = useState('');
-
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    
     const dispatch = useDispatch();
     const store = useSelector(store=> store.data);
-    console.log(store)
-    const json = require('./citylist.json');
+    const json = require('./country.json');
+
+    const showModal = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
    
 
-
-
     useEffect(()=>{
-      dispatch(axiosData(2643743))
+      dispatch(axiosData(630336))
     },[]); 
 
 
@@ -25,58 +37,65 @@ function App() {
     }
 
     function getPlace(json){
-        json.map(el=>{
-          if (el.name.includes(value)&& dispatch(axiosData(el.id))){
-            console.log('Найдено!')
-          }
-          else{
-            console.log('Не найдено!')
-          }
-        } );
-        setValue('')
-      
+     let res =  json.filter(city=>(city.name.toLowerCase()==value.toLowerCase()))
+     res.length>0? dispatch(axiosData(res[0].id)):message.error('такой страны в базе нет');
+     setValue('');
+
     }
 
-
-  
-  
+    const handleSubmit = event => {
+      event.preventDefault();
+      getPlace(json)
+    };
 
   return (
   
         <div className="App ">
-        { store && store.map(city=>{
+       <form onSubmit={handleSubmit} >
+       { store && store.map(city=>{
           return (  
-            <div className='container '>
+            <div className='container ' key={city.id}>
              
                 <div className='city'>
                   <div className='content'>
-                    <div>Введите название страны, погоду которой хотите узнать.</div>
+                  
                      <div className='input'>
-                     <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" />
-                    
+                     
+                   <Input required placeholder="Например : Беларусь" value={value} onChange={(e)=>setValue(e.target.value)} type="text"/>
+    
+                <Button ghost  onClick={showModal}>Инфо</Button>
+                <Modal title="Страны, которые есть в базе данных:" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>   
+                  <p>Беларусь</p>
+                  <p>Россия</p>
+                  <p>Украина</p>
+                  <p>Латвия</p>
+                  <p>Литва</p>
+                  <p>Польша</p>
+                  <p>Италия</p>
+                  <p>Испания</p>
+                  <p>Франция</p>
+                  <p>Германия</p>
+                  <p>Турция</p> 
+                </Modal>
                      </div>
-                      <button onClick={()=>getPlace(json)} >Поиск</button>
+                      <Button type="submit" onSubmit={(e)=>handleSubmit(e)} onClick={()=>getPlace(json)}>Поиск</Button>
                   </div>
                   <h1>{city.name}</h1>
                   <h2>{Math.floor( city.main['temp'] - 273)}&#176;</h2>
                   <div className='weather'>
                     
                     <img src={getImage(city.weather[0].icon)} alt="" /> 
-                    
-
                     <div>
                      {city.weather[0].main} 
                     </div>
                   </div>
-                </div>
-              {/*   <hr className='vertical'/> */}
+                  <p className='text'>API взято с сайта <a href="https://openweathermap.org/api"> https://openweathermap.org/api</a></p>
 
-             
-              
-              
+                </div>
               </div>
               )
                 })}
+       </form>
         </div>
   
 
@@ -85,14 +104,3 @@ function App() {
 }
 
 export default App;
-
-
-{/*     <h2 ></h2>
-                 Weather: 
-                   
-                
-
-              
-                    fills like:
-                    min: {Math.ceil( city.main['temp_min'] - 273)} &#176;
-                    max: {Math.ceil( city.main['temp_max'] - 273)} &#176; */}
